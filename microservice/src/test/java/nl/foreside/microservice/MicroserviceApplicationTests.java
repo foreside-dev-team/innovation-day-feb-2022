c675e900beefb99e8944fea0255c653ade8ac0f9
Adrian Yankov <adrian.yankov@tno.nl>
2022-01-29 22:18:09 +0100

    added java boilerplate microservice

---
+++ microservice/src/test/java/nl/foreside/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.foreside.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
